af99573dfc0c6d7d8754a2e01ae3167fc75f55a5
Samuel Ramos <alu0100815146@ull.edu.es>
2016-12-11 11:12:24 +0000

    Añadiendo ejercico par

--- todos_iguales.pl
+++ todos_iguales.pl
@@ -1,3 +1,3 @@
-todos_iguales([]). % Lista vacía, no hay elementos, se devuelve true.
-todos_iguales([_]). % Se ignore la lista unitaria.
+todos_iguales([]).                                    % Lista vacía, no hay elementos, se devuelve true.
+todos_iguales([_]).                                   % Se ignore la lista unitaria.
 todos_iguales([H|T]):- todos_iguales(T), member(H,T). %Se hace la llamada pasando la cola y se comprueba si H es miembro de T.
