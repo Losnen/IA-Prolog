8ec749838589215a26e4f8b156b0619e7ad5a8e7
Samuel Ramos <alu0100815146@ull.edu.es>
2016-12-12 10:10:57 +0000

    Eliminando comentarios

--- mcd.pl
+++ mcd.pl
@@ -1,10 +1,10 @@
-mcd(X, X, X).   % El maximo comun divisor de X y X, es X, Devuelve X cuando el valor de A y B sea X.
-mcd(0, A, 1).   % El maximo comun divisor de 0 y A, es 1.
+mcd(X, X, X).
+mcd(0, A, 1).
 mcd(A, B, Z):-
-  A < B,        % El maximo comun divisor de A y B y A < B.
-  mcd(B,A,Z).   % Es lo mismo que mcd(B, A).
+  A < B,
+  mcd(B,A,Z).
 
 mcd(A, B, Z):-
-  A >= B,       % Si A es mayor o igual que B, entonces podemos calcular el MCD.
-  A1 is A-B,    % A1 le asignamos A - B
-  mcd(A1,B,Z).  % Hacemos la llamada recursiva.
+  A >= B,
+  A1 is A-B,
+  mcd(A1,B,Z).
